3e24ac60786d8f666935d7e22fd8ebf62bd86992
Nic <85948162+nicguzz@users.noreply.github.com>
2023-03-15 18:04:24 +0100

    changes in html

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,8 +43,8 @@ const Navbar = (props) => {
         <div className="col-start-2 col-end-5">
           <form onSubmit={props.searchMovies}>
             <label
-              for="default-search"
-              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
             >
               Search
             </label>
@@ -52,16 +52,16 @@ const Navbar = (props) => {
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
                   aria-hidden="true"
-                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                   ></path>
                 </svg>
