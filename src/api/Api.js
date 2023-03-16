11ed9802508586b5b747cdd14a453858639f6337
Nic <85948162+nicguzz@users.noreply.github.com>
2023-03-16 17:51:05 +0100

    api keys hidden

---
+++ src/api/Api.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.themoviedb.org/3";
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
+
+// endpoint para las imagenes
+export const URL_IMAGE = "https://image.tmdb.org/t/p/original";
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -80,9 +80,10 @@ const Navbar = (props) => {
 
               <button
                 type="submit"
-                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white absolute right-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                onClick={clearSearch}
               >
-                Search
+                X
               </button>
             </div>
           </form>
