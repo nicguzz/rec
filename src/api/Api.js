1a21c14e677fae83466d0e87931f23b57047dd59
Nic <85948162+nicguzz@users.noreply.github.com>
2023-03-16 18:22:43 +0100

    api key test

--- src/api/Api.js
+++ src/api/Api.js
@@ -4,3 +4,5 @@ export const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
 
 // endpoint para las imagenes
 export const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+
+console.log(API_KEY);
